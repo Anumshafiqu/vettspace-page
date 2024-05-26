14110943ffe1cca339f81d90a313026351f1b204
anum shafique <anmshafique704@gmail.com>
2024-05-26 23:30:56 +0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// improts modules
+import { ButtonModule } from 'primeng/button';
+import { StepsModule } from 'primeng/steps';
+import { CardModule } from 'primeng/card';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    StepsModule,
+    CardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
